e621f03932cdb787328db120fd7b3bdc4bd55c1a
Julio Paim <julio.paim@klassmatt.com>
2026-01-13 13:53:39 -0300

    ajuste na logica de verificação da entidade

--- lib/rotas/GET/procuraEntidade.dart
+++ lib/rotas/GET/procuraEntidade.dart
@@ -24,15 +24,23 @@ Future<Response> procuraEntidade(Request request) async {
   bool temFiltros = false;
 
   for (var campo in camposValidos) {
-    if (data.containsKey(campo) && data[campo] != null && data[campo].toString().isNotEmpty) {
-      temFiltros = true;
-      } 
-    if (['id', 'rt', 'fundado'].contains(campo)) {
-        query = query.eq(campo, data[campo]);
-      }else {
-        query = query.ilike(campo, '%${data[campo]}%');
-      }
-      }
+  if (!data.containsKey(campo) ||
+      data[campo] == null ||
+      data[campo].toString().isEmpty) {
+    continue;
+  }
+
+  temFiltros = true;
+
+  if (['id', 'rt'].contains(campo)) {
+    query = query.eq(campo, data[campo]);
+  } else if (campo == 'fundado') {
+    query = query.eq(campo, data[campo]); // se for date exata
+  } else {
+    query = query.ilike(campo, '%${data[campo]}%');
+  }
+}
+
   
   if (!temFiltros) {
     return Response.badRequest(
