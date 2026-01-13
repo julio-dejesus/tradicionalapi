69eeebeed4a1aee582b7be5a74b5e54c92c6d0c4
Julio Paim <julio.paim@klassmatt.com>
2026-01-13 13:37:59 -0300

    ajustados logs e funcionalidade que salva valores da ultima requisicao

--- lib/rotas/GET/procuraEventos.dart
+++ lib/rotas/GET/procuraEventos.dart
@@ -41,7 +41,12 @@ Future<Response> procuraEventos(Request request) async {
     );
   }
 
-  await supabase.rpc("atualiza_ultima_requisicao");
+    try {
+    await supabase.rpc('atualiza_ultima_requisicao');
+  } catch (e, stack) {
+    print('ERRO na RPC atualiza_ultima_requisicao: $e');
+    print(stack);
+  }
 
   final result = await query;
 
