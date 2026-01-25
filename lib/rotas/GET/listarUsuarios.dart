183c09208dc62c14fa6f73f3795001adc60fa2ca
Julio <juliopaimgdf@gmail.com>
2026-01-25 17:29:14 -0300

    ajuste na classe de pesquisa de usuario

--- lib/rotas/GET/listarUsuarios.dart
+++ lib/rotas/GET/listarUsuarios.dart
@@ -3,21 +3,54 @@ import 'package:shelf/shelf.dart';
 import '../../supabase_client.dart';
 
 Future<Response> listarUsuarios(Request request) async{
+final params = request.url.queryParameters;
 
-    try {
+dynamic query = supabase
+      .from('usuarios')
+      .select('id, nome, login, email, senha, admin');
+
+final camposValidos = [
+    'id',
+    'nome',
+    'login',
+    'email',
+    'admin',
+  ];
+
+bool temFiltros = false;
+
+for (var campo in camposValidos) {
+  final valor = params[campo];
+
+  if (valor == null || valor.isEmpty) continue;
+
+  temFiltros = true;
+
+  if (['id', 'admin'].contains(campo)) {
+    query = query.eq(campo, valor);
+  } 
+  else {
+    query = query.ilike(campo, '%$valor%');
+  }
+}
+
+if (!temFiltros) {
+    query = query
+        .order('id', ascending: false)
+        .limit(10);
+  }
+
+try {
     await supabase.rpc('atualiza_ultima_requisicao');
   } catch (e, stack) {
     print('ERRO na RPC atualiza_ultima_requisicao: $e');
     print(stack);
   }
 
-  final result = await supabase
-  .from('usuarios')
-  .select('id, nome, login, email, senha, admin');
+  final result = await query;
 
   return Response.ok(
     jsonEncode(result),
     headers: {'Content-Type': 'application/json'},
   );
-
 }
